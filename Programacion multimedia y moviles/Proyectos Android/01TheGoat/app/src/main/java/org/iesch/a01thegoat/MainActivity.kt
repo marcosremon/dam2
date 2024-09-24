5c88b92d7a368424e3774b9933b328db36a9256b
unknown <mremonb@iesch.org>
2024-09-24 18:25:27 +0200

    fin clase martes

--- Programacion multimedia y moviles/Proyectos Android/01TheGoat/app/src/main/java/org/iesch/a01thegoat/MainActivity.kt
+++ Programacion multimedia y moviles/Proyectos Android/01TheGoat/app/src/main/java/org/iesch/a01thegoat/MainActivity.kt
@@ -1,10 +1,14 @@
 package org.iesch.a01thegoat
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +20,10 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        //1 - tomamos el control sobre las vistas que vamos a necesitar en la Activity
+        val resultText = findViewById<TextView>(R.id.tvRespuesta)
+        val calculateButton = findViewById<Button>(R.id.btnCalcular)
+        val ageEdit = findViewById<EditText>(R.id.etEdad)
     }
 }
