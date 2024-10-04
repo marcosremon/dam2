8d14c99ee8795e3181722ee65b41643a3518a436
unknown <mremonb@iesch.org>
2024-10-04 21:15:25 +0200

    a

---
+++ Programacion de servicios y procesos/UD1-2 - Iniciacion/unidad2/src/main/java/org/iesch/ad/Main.java
@@ -0,0 +1,14 @@
+package org.iesch.ad;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> a = new ArrayList<>(Arrays.asList("a", "b", "c", "d"));
+        for (int i = 0; i < a.size(); i++) {
+            so
+        }
+    }
+}
