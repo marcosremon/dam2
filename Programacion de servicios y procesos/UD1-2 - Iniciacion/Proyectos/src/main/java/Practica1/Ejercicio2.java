76600bcb1a316634798e727726cf037fa9223e01
marcosremon <mremonb@iesch.org>
2024-10-04 14:15:52 +0200

    a

--- Programacion de servicios y procesos/UD1-2 - Iniciacion/Proyectos/src/main/java/Practica1/Ejercicio2.java
+++ Programacion de servicios y procesos/UD1-2 - Iniciacion/Proyectos/src/main/java/Practica1/Ejercicio2.java
@@ -6,43 +6,42 @@ import java.util.List;
 
 public class Ejercicio2 {
     public static void main(String[] args) throws InterruptedException {
-        long maxWaitTime = 2 * 1000;
-        long programDuration = 16 * 1000;
+        long maxWaitTime = 320000000 * 1000;
         long startTime = System.currentTimeMillis();
 
-        Thread childThread = new Thread(() -> {
-            String[] messages = { "Programas", "Procesos", "Servicios", "Hilos" };
+        Thread th = new Thread(() -> {
+            String[] messages = {"Programas", "Procesos", "Servicios", "Hilos"};
             try {
                 for (int i = 0; i < messages.length; i++) {
-                    System.out.println("Hilo: " + Thread.currentThread().getName() + ". " + messages[i]);
+                    System.out.println("hilo: " + Thread.currentThread().getName() + ". " + messages[i]);
                     Thread.sleep(4000);
                 }
             } catch (InterruptedException e) {
-                System.out.println("Hilo: " + Thread.currentThread().getName() + " fue interrumpido. Terminando " +
-                        "rápido.");
+                System.out.println("hilo: " + Thread.currentThread().getName() + " fue interrumpido. Terminando " +
+                        "rapido.");
                 for (int i = 1; i < messages.length; i++) {
-                    System.out.println("Hilo: " + Thread.currentThread().getName() + ". " + messages[i]);
+                    System.out.println("hilo: " + Thread.currentThread().getName() + ". " + messages[i]);
                 }
             } finally {
-                System.out.println("Hilo: " + Thread.currentThread().getName() + ". *** Finalizado ***");
+                System.out.println("hilo: " + Thread.currentThread().getName() + ". *** finalizado ***");
             }
         });
-        childThread.start();
+        th.start();
 
-        System.out.println("Hilo: main. Tiempo de espera: " + (maxWaitTime / 1000) + "s");
-        while (childThread.isAlive() && (System.currentTimeMillis() - startTime) < maxWaitTime) {
-            System.out.println("Hilo: main. Esperando a que el hilo " + childThread.getName() + " termine");
+        System.out.println("hilo: main. Tiempo de espera: " + (maxWaitTime / 1000) + "s");
+        while (th.isAlive() && (System.currentTimeMillis() - startTime) < maxWaitTime) {
+            System.out.println("Hilo: main. Todavía esperando...");
             Thread.sleep(1000);
         }
 
-        if (childThread.isAlive()) {
-            System.out.println("Hilo: main. Cansado de esperar. Interrumpiendo el hilo " + childThread.getName());
-            childThread.interrupt();
+        if (th.isAlive()) {
+            System.out.println("hilo: main. Cansado de esperar. Interrumpiendo el hilo " + th.getName());
+            th.interrupt();
         }
-        childThread.join();
+        th.join();
 
         long endTime = System.currentTimeMillis();
-        System.out.println("Hilo: main. *** Finalizado. Tiempo de ejecución: " + (endTime - startTime) / 1000 +
-                "s ***");
+        System.out.println("hilo: main. *** Finalizado. Tiempo de ejecución: " + (endTime - startTime) / 1000 +
+                "s. ***");
     }
 }
